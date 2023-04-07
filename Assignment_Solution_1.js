dd87530e019ba9f9f214783669e3ffb8590421d1
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-04-07 19:34:47 +0500

    initian commit

--- Assignment_Solution_1.js
+++ Assignment_Solution_1.js
@@ -5,11 +5,11 @@ let firstNumber = Number(prompt("Enter first number"));
 let secondNumber = Number(prompt("Enter second number"));
 
 if (firstNumber > secondNumber) {
-console.log(firstNumber + " is the larger number");
+  console.log(firstNumber + " is the larger number");
 } else if (secondNumber > firstNumber) {
-console.log(secondNumber + " is the larger number");
+  console.log(secondNumber + " is the larger number");
 } else {
-console.log("Sorry, the numbers are equal");
+  console.log("Sorry, the numbers are equal");
 }
 
 /*
@@ -24,11 +24,11 @@ Output : The sign is -
 let number = Number(prompt("Enter a number, positive or negative"));
 
 if (number > 0) {
-alert("The sign is + ");
+  alert("The sign is + ");
 } else if (number < 0) {
-alert("The sign is - ");
+  alert("The sign is - ");
 } else {
-alert("The number has a value of 0");
+  alert("The number has a value of 0");
 }
 
 // 3. Write a JavaScript program that accepts five numbers in five prompts and
@@ -41,17 +41,17 @@ let numb4 = Number(prompt("Enter Fourth Number"));
 let numb5 = Number(prompt("Enter Fifth Number"));
 
 if (numb1 > numb2 && numb1 > numb3 && numb1 > numb4 && numb1 > numb5) {
-    console.log(`${numb1} is the largest number.`);
+  console.log(`${numb1} is the largest number.`);
 } else if (numb2 > numb1 && numb2 > numb3 && numb2 > numb4 && numb2 > numb5) {
-    console.log(`${numb2} is the largest number.`);
+  console.log(`${numb2} is the largest number.`);
 } else if (numb3 > numb1 && numb3 > numb2 && numb3 > numb4 && numb3 >= numb5) {
-    console.log(`${numb3} is the largest number.`);
+  console.log(`${numb3} is the largest number.`);
 } else if (numb4 > numb1 && numb4 > numb2 && numb4 > numb3 && numb4 > numb5) {
-    console.log(`${numb4} is the largest number.`);
+  console.log(`${numb4} is the largest number.`);
 } else if (numb5 > numb1 && numb5 > numb2 && numb5 > numb3 && numb5 > numb4) {
-    console.log(`${numb5} is the largest number.`);
+  console.log(`${numb5} is the largest number.`);
 } else {
-    console.log(`They are all equal`);
+  console.log(`They are all equal`);
 }
 
 // 4 Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it
@@ -62,23 +62,17 @@ if (numb1 > numb2 && numb1 > numb3 && numb1 > numb4 && numb1 > numb5) {
 // "1 is odd"
 // "2 is even"
 
-
 for (let num = 0; num < 15; num++) {
-
-    if (num % 2 === 0) {
-        console.log(num + " " + "is even");
-    }
-    else if (num % 2 !== 0) {
-        console.log(num + " " + "is odd");
-    }
-
+  if (num % 2 === 0) {
+    console.log(num + " " + "is even");
+  } else if (num % 2 !== 0) {
+    console.log(num + " " + "is odd");
+  }
 }
-
 
 // 5. Write a JavaScript program which computes the average marks of the
 // following students Then, this average is used to determine the corresponding
 // grade.
-
 
 const marks1 = Number(prompt("Enter the first mark:"));
 const marks2 = Number(prompt("Enter the second mark:"));
@@ -86,48 +80,45 @@ const marks3 = Number(prompt("Enter the third mark:"));
 const marks4 = Number(prompt("Enter the fourth mark:"));
 const marks5 = Number(prompt("Enter the fifth mark:"));
 
-const sum = marksOfSubject1 + marksOfSubject2 + marksOfSubject3 + marksOfSubject4 + marksOfSubject5;
+const sum =
+  marksOfSubject1 +
+  marksOfSubject2 +
+  marksOfSubject3 +
+  marksOfSubject4 +
+  marksOfSubject5;
 const avg = sum / 5;
 
 let grade;
 if (avg < 60) {
-    grade = 'F';
+  grade = "F";
 } else if (avg < 70) {
-    grade = 'D';
+  grade = "D";
 } else if (avg < 80) {
-    grade = 'C';
+  grade = "C";
 } else if (avg < 90) {
-    grade = 'B';
+  grade = "B";
 } else {
-    grade = 'A';
+  grade = "A";
 }
 
 console.log(`The average mark is ${avg} and the grade is ${grade}.`);
 
-
-// // 6. Write a JavaScript program which iterates the integers from 1 to 100. But for
+// 6. Write a JavaScript program which iterates the integers from 1 to 100. But for
 // multiples of three print "Fizz" instead of the number and for the multiples of five
 // print "Buzz". For numbers which are multiples of both three and five print
 // "FizzBuzz".
 
 for (let num = 1; num < 100; num++) {
-
-    if (num % 3 === 0 && num % 5 === 0) {
-        console.log("FizzBuzz");
-    }
-
-    else if (num % 3 === 0) {
-        console.log("Fizz");
-    }
-    else if (num % 5 === 0) {
-        console.log("Buzz");
-    }
-
-    else {
-        console.log(num);
-    }
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  }
 }
-
 
 // Write a JavaScript program to construct the following pattern, using a nested
 // for loop.
@@ -137,17 +128,12 @@ for (let num = 1; num < 100; num++) {
 // * * * *
 // * * * * *
 
-
 let n = 5;
 
 for (let i = 0; i < n; i++) {
-    let stars = " ";
-    for (let j = 0; j < i; j++) {
-        stars += "*"
-    }
-    console.log(stars);
-
+  let stars = " ";
+  for (let j = 0; j < i; j++) {
+    stars += "*";
+  }
+  console.log(stars);
 }
-
-
-
